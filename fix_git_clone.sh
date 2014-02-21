3985f9897ae7bec4c2ba929502b9664b18203175
tuvistavie <tuvistavie@gmail.com>
2014-02-22 02:58:51 +0900

    Update fix_git_clone to avoid checking out when not needed.

--- fix_git_clone.sh
+++ fix_git_clone.sh
@@ -2,5 +2,10 @@
 
 data_dir=$1
 
-find $data_dir -name ".git" -not -path "*/.bundle/*" -exec sh -c "cd {}/.. && git checkout master && git checkout ." {} \;
+find $data_dir -name ".git" -not -path "*/.bundle/*" -exec sh -c "
+  cd {}/.. &&
+  git branch | grep deploy &&
+  git checkout master &&
+  git checkout . &&
+  git branch -d deploy" {} \;
 
